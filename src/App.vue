bec27461a0ebfb24a1ca5dea0910e13f5412bbd2
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-04-22 10:29:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+
+  <div class="container">
+    <div class="row row-cols-2">
+      <div class="col">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint obcaecati, delectus officiis magnam ipsum minima perspiciatis soluta, at, nesciunt possimus quam iure harum tempora sequi itaque id hic impedit earum.
+      </div>
+      <div class="col">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint obcaecati, delectus officiis magnam ipsum minima perspiciatis soluta, at, nesciunt possimus quam iure harum tempora sequi itaque id hic impedit earum.
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+</style>
